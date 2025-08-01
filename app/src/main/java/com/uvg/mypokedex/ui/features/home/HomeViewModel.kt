4b580b7f87be96d44e10db964598a2cc3141938e
Pablownski <sal24374@uvg.edu.gt>
2025-08-01 13:58:29 -0600

    HomeView modelaje

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
@@ -1,2 +1,13 @@
 package com.uvg.mypokedex.ui.features.home
 
+import com.uvg.mypokedex.data.Pokemon
+
+class HomeViewModel{
+    fun getPokemonList(): List<Pokemon>{
+        return listOf(
+            Pokemon(1, "bulbasaur"),
+            Pokemon(4, "Charmander"),
+            Pokemon(150, "Mewtwo")
+        )
+    }
+}
