f2ce480df020c8935a59d38f870c7d4655a5fef2
Pablownski <sal24374@uvg.edu.gt>
2025-08-01 21:11:38 -0600

    HomeScreen

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeScreen.kt
@@ -1,2 +1,26 @@
 package com.uvg.mypokedex.ui.features.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.uvg.mypokedex.ui.components.PokemonCard
+
+@Composable
+fun HomeScreen(viewModel: HomeViewModel = HomeViewModel()){
+    val pokemonList = viewModel.getPokemonList()
+
+    LazyVerticalGrid (
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        columns = GridCells.Fixed(2)
+    ) {
+        items(pokemonList) {
+            pokemon ->
+            PokemonCard(pokemon)
+        }
+    }
+}
+
+
