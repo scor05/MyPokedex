518d19e371e5f4645537be254abd22222d7a4f92
Pablownski <sal24374@uvg.edu.gt>
2025-08-01 22:35:32 -0600

    aaaaaaaaaaaaa

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
@@ -5,9 +5,19 @@ import com.uvg.mypokedex.data.Pokemon
 class HomeViewModel{
     fun getPokemonList(): List<Pokemon>{
         return listOf(
+            Pokemon(1, "Messi"),
+            Pokemon(4, "Charmander"),
+            Pokemon(3, "Ivysaur"),
+            Pokemon(150, "Mewtwo"),
+            Pokemon(7, "CR7"),
+            Pokemon(4, "Charmander"),
+            Pokemon(150, "Mewtwo"),
             Pokemon(1, "bulbasaur"),
             Pokemon(4, "Charmander"),
-            Pokemon(150, "Mewtwo")
+            Pokemon(150, "Mewtwo"),
+            Pokemon(1, "bulbasaur"),
+            Pokemon(4, "Charmander"),
+            Pokemon(150, "Mewtwo"),
         )
     }
 }
