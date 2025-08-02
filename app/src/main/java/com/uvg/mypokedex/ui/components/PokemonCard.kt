3cea3ec84ee3f94beaa18c1e54a2f6d3d93d5888
Juan Daniel Ordoñez <ord24979@uvg.edu.gt>
2025-08-01 20:17:39 -0600

    Modificaciones al archivo PokemonCard para la parte 4 de Lab

--- app/src/main/java/com/uvg/mypokedex/ui/components/PokemonCard.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/components/PokemonCard.kt
@@ -1,4 +1,40 @@
 package com.uvg.mypokedex.ui.components
 
-// A
 
+@Composable
+fun PokemonCard(pokemon: Pokemon) {
+    val typeColor = Color.Gray // Puedes cambiar según tipo
+
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = typeColor)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            // Imagen
+            AsyncImage(
+                model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png",
+                contentDescription = "Imagen de ${pokemon.name}",
+                modifier = Modifier.size(120.dp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            // Nombre
+            Text(
+                text = pokemon.name.replaceFirstChar { it.uppercaseChar() },
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
