01ff7dfc766509655a4a020fc6b2ab855fecac0c
Pablownski <sal24374@uvg.edu.gt>
2025-08-22 14:49:05 -0600

    Atributos para el pokemon y stats

--- app/src/main/java/com/uvg/mypokedex/data/Pokemon.kt
+++ app/src/main/java/com/uvg/mypokedex/data/Pokemon.kt
@@ -2,5 +2,13 @@ package com.uvg.mypokedex.data
 
 data class Pokemon(
     val id: Int,
-    val name: String
+    val name: String,
+    val type: List<String>,
+    val weight: Float,
+    val stats: List<Stat>,
+    val height: Float
+)
+data class Stat(
+    val name: String,
+    val value: Int
 )
