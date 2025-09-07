57f384bf7555365678e2fcd9571a35670559c27b
Santiago Cordero <santicordero28@gmail.com>
2025-09-07 10:31:31 -0600

    Terminado la función de loadMorePokemon y parseado de JSON a lista de Pokemon.

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
@@ -1,9 +1,13 @@
 package com.uvg.mypokedex.ui.features.home
 
 import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.layout.add
 import androidx.compose.runtime.mutableStateListOf
 import com.uvg.mypokedex.data.Pokemon
 import com.uvg.mypokedex.data.Stat
+import kotlinx.serialization.*
+import org.json.JSONObject
 import java.util.Locale
 
 private val _favoritePokemons = mutableStateListOf<String>()
@@ -11,10 +15,56 @@ class HomeViewModel(context: Context) {
 
     private var currentPage = 0
 
-    val currentJson = generateFileName(currentPage)
 
-    fun loadPokemonFromJson(): List<Pokemon> {
-        return emptyList() // TODO: hacerlo
+    fun loadMorePokemon(): List<Pokemon> {
+        try {
+            currentPage++
+            val newJson = generateFileName(currentPage)
+            val jsonObject = JSONObject(newJson)
+            val results = jsonObject.getJSONArray("items")
+            val pokemonList = mutableListOf<Pokemon>()
+
+            for (i in 0 until results.length()) {
+                val pokemonJson = results.getJSONObject(i)
+
+                // los JSONObject tienen métodos get*TIPO*(string) para retornar
+                // el dato correspondiente a esa etiqueta
+                val id = pokemonJson.getInt("id")
+                val name = pokemonJson.getString("name")
+                val height = pokemonJson.getDouble("height").toFloat()
+                val weight = pokemonJson.getDouble("weight").toFloat()
+
+                val typesArray = pokemonJson.getJSONArray("types")
+                val types = mutableListOf<String>()
+                for (j in 0 until typesArray.length()) {
+                    types.add(typesArray.getString(j))
+                }
+
+                val statsArray = pokemonJson.getJSONArray("stats")
+                val stats = mutableListOf<Stat>()
+                for (j in 0 until statsArray.length()) {
+                    val statJson = statsArray.getJSONObject(j)
+                    val statName = statJson.getString("name")
+                    val statValue = statJson.getInt("value")
+                    stats.add(Stat(name = statName, value = statValue))
+                }
+
+                val pokemon = Pokemon(
+                    id = id,
+                    name = name,
+                    type = types,
+                    height = height,
+                    weight = weight,
+                    stats = stats
+                )
+                pokemonList.add(pokemon)
+            }
+
+            return pokemonList
+        } catch (e: Exception){
+            Log.d("HomeViewModel", e.message.toString())
+            return emptyList()
+        }
     }
 }
 
