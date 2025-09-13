0d959c677173313e45486700878761b06f02e666
Juan Salguero <sal24374@uvg.edu.gt>
2025-09-13 08:32:00 -0600

    Actualización de AppScreens.kt

--- app/src/main/java/com/uvg/mypokedex/navigation/AppScreens.kt
+++ app/src/main/java/com/uvg/mypokedex/navigation/AppScreens.kt
@@ -1,11 +1,15 @@
 package com.uvg.mypokedex.navigation
 
-sealed class AppScreens(val route: String) {
-    object HomeScreen : AppScreens("home_screen")
-    object FavoriteScreen : AppScreens("favorite_screen")
-    object DetailScreen : AppScreens("detail_screen")
-}
 
+sealed class AppScreens(val route: String) {
+
+    object HomeScreen : AppScreens("home")
+
+    object DetailScreen : AppScreens("detail/{itemId}") {
+        fun createRoute(itemId: String) = "detail/$itemId"
+    }
+
+    object SearchToolsDialog : AppScreens("search_tools_dialog")
 
 
 
