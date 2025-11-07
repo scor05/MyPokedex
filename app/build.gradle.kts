33458f025afdf609feffd955e4edca121fcd5082
Juan Daniel Ordoñez <ord24979@uvg.edu.gt>
2025-11-07 12:45:58 -0600

    exchange screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,4 +83,6 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
+
+    implementation(libs.androidx.compose.material.icons.extended)
 }
